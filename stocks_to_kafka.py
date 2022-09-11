da0a88498a38f0c70b10cb8df60d1c8d8c63b403
OctavioAlcazar55 <octavio.alcazar55@gmail.com>
2022-09-11 15:11:37 +0200

    Update producer file

--- stocks_to_kafka.py
+++ stocks_to_kafka.py
@@ -33,13 +33,15 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
 
-producer = Producer({'bootstrap.servers': 'http://ec2-44-204-8-116.compute-1.amazonaws.com:9092'})
+producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
 def on_open(ws):
     ws.send('{"type":"subscribe","symbol":"AAPL"}')
     ws.send('{"type":"subscribe","symbol":"AMZN"}')
     ws.send('{"type":"subscribe","symbol":"META"}')
     ws.send('{"type":"subscribe","symbol":"MSFT"}')
+    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+
 
 if __name__ == "__main__":
     #websocket.enableTrace(True)
